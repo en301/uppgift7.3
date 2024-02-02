972a9b8a57008fa978d591196c0ce388e26a56c5
en301 <emilsjostrand1000@gmail.com>
2024-02-02 12:47:29 +0100

    Add project files.

---
+++ uppgift7.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uppgift_7_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> storlek = new Dictionary<string, int>();
+            storlek["Sverige"] = 450;
+            storlek["Norge"] = 385;
+            storlek["Danmark"] = 43;
+            storlek["Finland"] = 338;
+
+            Console.WriteLine("Skriv ett land av Sverige, Norge, Danmark och Finland som du vill veta storleken på.");
+            string svar = Console.ReadLine();
+            Console.WriteLine($"{svar} är ungefär {storlek[svar]} 000 km^2 stort till ytan");
+            Console.ReadKey();
+        }
+    }
+}
